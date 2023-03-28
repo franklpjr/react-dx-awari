01017383f3b6b2f79b7ce975040d728d993d548e
Vinicius Fortunato <vinedfs@outlook.com>
2023-03-28 20:42:56 -0300

    feat: added product list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import ProductList from './components/ProductList';
 
 function App() {
+
+	const products = [
+		{
+			id: 0,
+			title: 'PS5',
+			price: 1000
+		}
+	];
+
   return (
     <div>
       <h1>Hello</h1>
-			<ProductList />
+			<ProductList products={products} />
     </div>
   );
 }
